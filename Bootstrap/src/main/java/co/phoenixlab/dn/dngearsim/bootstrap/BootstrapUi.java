40a6df0a6b49e3cb6c6b6ad9a87fd57745247139
Vincent Zhang <vzhang@purdue.edu>
2016-01-08 16:32:41 -0500

    Add UI controller for bootstrap

---
+++ Bootstrap/src/main/java/co/phoenixlab/dn/dngearsim/bootstrap/BootstrapUi.java
@@ -0,0 +1,41 @@
+package co.phoenixlab.dn.dngearsim.bootstrap;
+
+import javafx.beans.binding.Bindings;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+
+public class BootstrapUi {
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private Rectangle loadingBar;
+
+    @FXML
+    private Label statusText;
+
+    @FXML
+    void initialize() {
+        assert pane != null : "fx:id=\"pane\" was not injected: check your FXML file 'splash.fxml'.";
+        assert loadingBar != null : "fx:id=\"loadingBar\" was not injected: check your FXML file 'splash.fxml'.";
+        assert statusText != null : "fx:id=\"statusText\" was not injected: check your FXML file 'splash.fxml'.";
+
+    }
+
+    public void bindToTask(Task<?> task) {
+        assert task != null : "Task cannot be null, check invocation site";
+        statusText.textProperty().
+                bind(task.messageProperty());
+        loadingBar.widthProperty().
+                bind(Bindings.multiply(pane.widthProperty(), task.progressProperty()));
+    }
+
+    public void unbind() {
+        statusText.textProperty().unbind();
+        loadingBar.widthProperty().unbind();
+    }
+}
