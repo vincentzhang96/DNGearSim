b2a95b9fd0dd934423c1804274c0687b2a998892
Vincent Zhang <vzhang@purdue.edu>
2016-02-10 21:44:31 -0500

    Remove unnecessary pause, CD back to top

--- tools/buildexe.bat
+++ tools/buildexe.bat
@@ -6,9 +6,8 @@ RCEDIT64.exe /N DNGearSim.exe DNGearSim.ini
 RCEDIT64.exe /S DNGearSim.exe splash.bmp
 RCEDIT64.exe /I DNGearSim.exe icon.ico
 COPY "DNGearSim.exe" "../../runtime/DNGearSim.exe"
-
 CD ../..
+
 CD Bootstrap/target
 COPY "bootstrap.jar" "../../runtime/bootstrap.jar"
-
-PAUSE
+CD ../..
