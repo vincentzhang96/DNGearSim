19da2fa6f7a6d0d496d6df0d9dfb23d71c669718
Vincent Zhang <vzhang@purdue.edu>
2016-03-12 22:54:38 -0500

    Bootstrap handoff receiver

--- Patcher/src/main/java/co/phoenixlab/dn/dngearsim/patcher/Patcher.java
+++ Patcher/src/main/java/co/phoenixlab/dn/dngearsim/patcher/Patcher.java
@@ -1,10 +1,24 @@
 package co.phoenixlab.dn.dngearsim.patcher;
 
-public class Patcher {
+import co.phoenixlab.dn.dngearsim.bootstrap.Bootstrap;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Patcher implements co.phoenixlab.dn.dngearsim.bootstrap.BootstrapHandoff {
 
     public static void main(String[] args) {
 
     }
 
 
+    public Patcher() {
+
+    }
+
+    @Override
+    public void handOff(Bootstrap bootstrap, Stage mainStage, Scene mainScene) {
+        //  TODO
+        System.out.println("Potato");
+
+    }
 }
